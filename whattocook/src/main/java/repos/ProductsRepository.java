21923443db2ee43975909c10c1f35ba5ac860b1c
darmnn <52609185+darmnn@users.noreply.github.com>
2020-11-15 15:36:24 +0300

    Delete ProductsRepository.java

--- whattocook/src/main/java/repos/ProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package repos;
-
-import models.Products;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductsRepository extends CrudRepository<Products, Integer> {
-}
